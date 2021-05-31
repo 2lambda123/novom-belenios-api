afc22a8b4e03760fd3f9b32dc7732ea56c9ccabb
GeorgesMainville <gmainville@novominteractive.com>
2021-05-31 18:35:58 -0400

    Delete vote save

--- src/scripts/vote.sh
+++ src/scripts/vote.sh
@@ -6,7 +6,6 @@ belenios-tool () {
 
 PRIVCRED=$1
 BALLOT=$2
-BALLOT_FILE_PATH=$3
-DIR=$4
+DIR=$3
 
-belenios-tool vote --privcred <(echo "$PRIVCRED") --ballot <(echo "$BALLOT") --dir "$DIR" >> "$BALLOT_FILE_PATH"
+belenios-tool vote --privcred <(echo "$PRIVCRED") --ballot <(echo "$BALLOT") --dir "$DIR"
