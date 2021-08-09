6c033f3d6e73564bb493e11e77eb8c3232fdf009
GeorgesMainville <gmainville@novominteractive.com>
2021-08-09 16:06:13 -0400

    Add deploy sript

--- belenios-ballot-lambda/src/app.js
+++ belenios-ballot-lambda/src/app.js
@@ -1,37 +1,42 @@
 const { exec } = require('child_process');
 const { writeFileSync } = require('fs');
+const { default: ApiResponse } = require('./lib/ApiResponse');
 
 function executeVote(privCred, ballot, electionDir) {
   return new Promise((resolve) => {
     exec(`bash ${__dirname}/scripts/vote.sh ${privCred} ${ballot} ${electionDir}`, (error, stdout) => {
       if (error) {
-        resolve({ status: 'FAILED', payload: error });
+        resolve(new ApiResponse(500, JSON.stringify(error)));
       }
-      resolve({ status: 'OK', payload: stdout });
+      resolve(new ApiResponse(200, JSON.stringify({ ballot: stdout })));
     });
   });
 }
 
 async function createBallot(event) {
-  const {
-    privCred,
-    ballot,
-    election,
-    trustees,
-  } = JSON.parse(event.body);
+  try {
+    const {
+      privCred,
+      ballot,
+      election,
+      trustees,
+    } = JSON.parse(event.body);
 
-  const electionPath = '/tmp';
-  const privCredFilePath = `${electionPath}/privCred`;
-  const ballotFilePath = `${electionPath}/ballot`;
-  const electionFilePath = `${electionPath}/election.json`;
-  const trusteesFilePath = `${electionPath}/trustees.json`;
+    const electionPath = '/tmp';
+    const privCredFilePath = `${electionPath}/privCred`;
+    const ballotFilePath = `${electionPath}/ballot`;
+    const electionFilePath = `${electionPath}/election.json`;
+    const trusteesFilePath = `${electionPath}/trustees.json`;
 
-  writeFileSync(privCredFilePath, privCred);
-  writeFileSync(ballotFilePath, ballot);
-  writeFileSync(electionFilePath, election);
-  writeFileSync(trusteesFilePath, trustees);
+    writeFileSync(privCredFilePath, privCred);
+    writeFileSync(ballotFilePath, ballot);
+    writeFileSync(electionFilePath, election);
+    writeFileSync(trusteesFilePath, trustees);
 
-  return executeVote(privCredFilePath, ballotFilePath, electionPath);
+    return executeVote(privCredFilePath, ballotFilePath, electionPath);
+  } catch (error) {
+    return new ApiResponse(500, JSON.stringify(error));
+  }
 }
 
 exports.handler = createBallot;
