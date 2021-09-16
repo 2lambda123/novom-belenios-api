a4601092087b8797c7d64b0cbd150284ed4a0408
GeorgesMainville <gmainville@novominteractive.com>
2021-09-16 09:38:20 -0400

    Add deploy scripts

---
+++ belenios-api-lambda/scripts/undeploy.sh
@@ -0,0 +1,31 @@
+REGION=$1
+STAGE=$2
+
+# Exit when any command fails
+set -e
+
+if [ $STAGE = "production" ] || [ $STAGE = "demo" ]
+then
+while true; do
+    read -p "You are about to undeploy belenios-api in $STAGE. Do you want to continue ? [y/n]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+fi
+
+if [ $STAGE = "production" ]
+then
+while true; do
+    read -p "Really ? [y/n]" yn
+    case $yn in
+        [Yy]* ) break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
+fi
+
+serverless remove --stage $STAGE --region $REGION
