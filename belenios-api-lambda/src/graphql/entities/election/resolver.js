3f5a7d3a9461ce3ddb2640485fa68510c45d1760
GeorgesMainville <gmainville@novominteractive.com>
2021-10-06 12:24:07 -0400

    Add create election

--- belenios-api-lambda/src/graphql/entities/election/resolver.js
+++ belenios-api-lambda/src/graphql/entities/election/resolver.js
@@ -1,4 +1,5 @@
-import createElection from '../../../lib/belenios/admin/createElection';
+import openElection from '../../../lib/belenios/admin/openElection';
+import electionFilesToObject from '../../../lib/helpers/electionFilesToObject';
 import { Election } from '../../../models';
 
 const resolver = {
@@ -7,10 +8,12 @@ const resolver = {
     getElection: async (_, id) => Election.get(id),
   },
   Mutation: {
-    createElection: async () => {
-      const electionId = createElection();
+    createElection: async (_, { votersList, template }) => {
+      const electionId = openElection(votersList, template);
+      const electionFiles = electionFilesToObject(electionId);
       const election = {
         id: electionId,
+        files: electionFiles,
         status: 'OPEN',
       };
       await Election.put(election);
