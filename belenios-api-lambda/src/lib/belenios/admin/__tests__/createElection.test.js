45299170d020469b0bdbdec97326ff20a1b037b2
GeorgesMainville <gmainville@novominteractive.com>
2021-10-13 17:50:03 -0400

    delete election after test

--- belenios-api-lambda/src/lib/belenios/admin/__tests__/createElection.test.js
+++ belenios-api-lambda/src/lib/belenios/admin/__tests__/createElection.test.js
@@ -1,8 +1,10 @@
 import createElection from '../createElection';
+import deleteElection from '../deleteElection';
 
 describe('Tests createElection', () => {
   it('Should return an election id', async () => {
     const electionId = createElection();
+    deleteElection(electionId);
     expect(electionId).toBeDefined();
   });
 });
