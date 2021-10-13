abcba8c51d7a672e488ac4890e7e2e50e0cecbe7
GeorgesMainville <gmainville@novominteractive.com>
2021-10-13 17:48:32 -0400

    Add close election

--- belenios-api-lambda/src/lib/belenios/admin/closeElection.js
+++ belenios-api-lambda/src/lib/belenios/admin/closeElection.js
@@ -1,5 +1,5 @@
 import fs from 'fs';
-import { exec } from 'child_process';
+import { execSync } from 'child_process';
 import path from 'path';
 import {
   ELECTIONS_DIR,
@@ -9,50 +9,20 @@ import {
 } from '../global';
 import log from '../../logger/log';
 
-function executeCloseElection(privateKeysFileName,
-  partialDecryptionsFilePath,
-  resultFilePath,
-  electionDir,
-  callback) {
-  exec(`bash src/scripts/closeElection.sh ${privateKeysFileName} ${partialDecryptionsFilePath} ${resultFilePath} ${electionDir}`,
-    (error, stdout) => {
-      if (error) {
-        callback({ status: 'FAILED', error });
-        return;
-      }
-      const result = JSON.parse(stdout);
-      if (result && result.result) {
-        callback({ status: 'OK', payload: result.result });
-      } else {
-        callback({ status: 'FAILED', payload: 'Invalid results' });
-      }
-    });
-}
-
-function closeElection(electionId, callback) {
-  if (!callback) return;
-
+function closeElection(electionId) {
   try {
-    const electionDir = electionId ? path.join(ELECTIONS_DIR, electionId) : undefined;
-
-    if (!electionDir || !fs.existsSync(electionDir)) {
-      callback({ status: 'FAILED', error: `Election ${electionId} does not exist.` });
-      return;
-    }
-
+    const electionDir = path.join(ELECTIONS_DIR, electionId);
     const privateKeysFileName = path.join(electionDir, PRIV_KEYS_FILE_NAME);
     const partialDecryptionsFilePath = path.join(electionDir, PARTIAL_DECRYPTIONS_FILE_NAME);
     const resultFilePath = path.join(electionDir, RESULT_FILE_NAME);
-
-    executeCloseElection(privateKeysFileName,
-      partialDecryptionsFilePath,
-      resultFilePath,
-      electionDir,
-      callback);
+    const { result } = JSON.parse(
+      execSync(`bash src/scripts/closeElection.sh ${privateKeysFileName} ${partialDecryptionsFilePath} ${resultFilePath} ${electionDir}`).toString(),
+    );
+    return result;
   } catch (error) {
     log('error', error);
-    callback({ status: 'FAILED', error: error.message });
   }
+  return undefined;
 }
 
 export default closeElection;
