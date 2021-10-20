ec3aae2b5f8578671928e83c02101b4129ba81d0
GeorgesMainville <gmainville@novominteractive.com>
2021-10-20 14:07:15 -0400

    Add update to model

--- belenios-api-lambda/src/models/Model.js
+++ belenios-api-lambda/src/models/Model.js
@@ -4,6 +4,7 @@ import {
   GetCommand,
   PutCommand,
   QueryCommand,
+  UpdateCommand,
 } from '@aws-sdk/lib-dynamodb';
 import log from '../lib/logger/log';
 import dynamoDBDocumentClient from '../lib/dynamoDB/dynamoDBDocumentClient';
@@ -85,6 +86,29 @@ class Model {
     try {
       const { Items } = await dynamoDBDocumentClient.send(new QueryCommand(mergedParams));
       return Items;
+    } catch (error) {
+      log('error', JSON.stringify(error));
+    }
+
+    return null;
+  }
+
+  async update(id, item) {
+    const UpdateExpression = `set ${Object.keys(item).map((k) => `#${k} = :${k}`).join(', ')}`;
+    const ExpressionAttributeNames = Object.entries(item).reduce((acc, cur) => ({ ...acc, [`#${cur[0]}`]: cur[0] }), {});
+    const ExpressionAttributeValues = Object.entries(item).reduce((acc, cur) => ({ ...acc, [`:${cur[0]}`]: cur[1] }), {});
+
+    const params = {
+      TableName: this.tableName,
+      Key: { id },
+      UpdateExpression,
+      ExpressionAttributeNames,
+      ExpressionAttributeValues,
+    };
+
+    try {
+      const data = await dynamoDBDocumentClient.send(new UpdateCommand(params));
+      return data;
     } catch (error) {
       log('error', JSON.stringify(error));
     }
