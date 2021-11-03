b3edaf66764090237e3651981429c27752edf249
GeorgesMainville <gmainville@novominteractive.com>
2021-11-03 10:47:43 -0400

    Delete coments

--- src/lib/dynamoDB/dynamoDBDocumentClient.js
+++ src/lib/dynamoDB/dynamoDBDocumentClient.js
@@ -2,22 +2,17 @@ import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import dynamoDBClient from './dynamoDBClient';
 
 const marshallOptions = {
-  // Whether to automatically convert empty strings, blobs, and sets to `null`.
-  convertEmptyValues: false, // false, by default.
-  // Whether to remove undefined values while marshalling.
-  removeUndefinedValues: false, // false, by default.
-  // Whether to convert typeof object to map attribute.
-  convertClassInstanceToMap: false, // false, by default.
+  convertEmptyValues: false,
+  removeUndefinedValues: false,
+  convertClassInstanceToMap: false,
 };
 
 const unmarshallOptions = {
-  // Whether to return numbers as a string instead of converting them to native JavaScript numbers.
-  wrapNumbers: false, // false, by default.
+  wrapNumbers: false,
 };
 
 const translateConfig = { marshallOptions, unmarshallOptions };
 
-// Create the DynamoDB Document client.
 const dynamoDBDocumentClient = DynamoDBDocumentClient.from(dynamoDBClient, translateConfig);
 
 export default dynamoDBDocumentClient;
